acea7ccb46c1019fe70101508597aaee9cce4c63
Mamina Papa <ada.norrsken@gmail.com>
2022-01-03 04:17:49 +0300

    Add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+n = Math.abs(n)  
+let k = n
+let p = n.toString()
+let s = 0
+for (let i = 0; i < p.length; i++) {
+ k = Math.floor(n%10)
+ n = n/10
+ s = s+ k + ""
+}
+return s
 }
